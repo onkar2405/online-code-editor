9596031b491cd7226c069f6174b00f752c6310ea
onkar2405 <onkarvaidyad789@gmail.com>
2025-02-26 23:33:50 +0530

    Final touch

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -3,56 +3,102 @@ import "../styles/CodeEditor.css";
 import SandBoxContainer from "./layout/SandBoxContainer";
 import { Editor } from "@monaco-editor/react";
 import { FaCss3 } from "react-icons/fa6";
+import { useState } from "react";
 
 export default function CodeEditor() {
   const options = {
     fontSize: "16px",
   };
+
+  const [jsCode, setJsCode] = useState("");
+  const [cssCode, setCssCode] = useState("");
+  const [htmlCode, setHtmlCode] = useState("");
+
+  function compileCode() {
+    const iframe = document.getElementById("output-frame");
+    const doc = iframe.contentWindow.document;
+
+    doc.open();
+    doc.write(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>${cssCode}</style>
+      </head>
+      <body>
+        ${htmlCode}
+        <script>${jsCode}</script>
+      </body>
+      </html>
+    `);
+    doc.close();
+  }
+
   return (
     <div className="main-container">
-      <div className="title-section">
-        <FaCode className="title-icon" />
-        <h1 className="project-title">Code Editor</h1>
+      <div className="title-container">
+        <div className="title-section">
+          <FaCode className="title-icon" />
+          <h1 className="project-title">Code Editor</h1>
+        </div>
+        <button className="run-btn" onClick={() => compileCode()}>
+          Run Code
+        </button>
       </div>
 
       <div className="editor-wrapper">
-        <SandBoxContainer title={"HTML"} Icon={FaHtml5}>
-          <Editor
-            options={options}
-            width="100%"
-            theme={"vs-dark"}
-            language="html"
-            defaultLanguage="html"
-            onChange={(value) => {
-              console.log(value);
-            }}
-          />
-        </SandBoxContainer>
+        <div className="height-250">
+          <SandBoxContainer title={"HTML"} Icon={FaHtml5}>
+            <Editor
+              options={options}
+              width="100%"
+              theme={"vs-dark"}
+              language="html"
+              defaultLanguage="html"
+              onChange={(value) => {
+                setHtmlCode(value);
+              }}
+            />
+          </SandBoxContainer>
+        </div>
+        <div className="height-250">
+          <SandBoxContainer title={"CSS"} Icon={FaCss3}>
+            <Editor
+              options={options}
+              width="100%"
+              theme={"vs-dark"}
+              language={"css"}
+              defaultLanguage="css"
+              onChange={(value) => {
+                setCssCode(value);
+              }}
+            />
+          </SandBoxContainer>
+        </div>
+        <div className="height-250">
+          <SandBoxContainer title={"JavaScript"} Icon={FaJs}>
+            <Editor
+              options={options}
+              width="100%"
+              theme={"vs-dark"}
+              language={"javascript"}
+              defaultLanguage="javascript"
+              onChange={(value) => {
+                setJsCode(value);
+              }}
+            />
+          </SandBoxContainer>
+        </div>
+      </div>
 
-        <SandBoxContainer title={"CSS"} Icon={FaCss3}>
-          <Editor
-            options={options}
-            width="100%"
-            theme={"vs-dark"}
-            language={"css"}
-            defaultLanguage="css"
-            onChange={(value) => {
-              console.log(value);
-            }}
-          />
-        </SandBoxContainer>
-        <SandBoxContainer title={"JavaScript"} Icon={FaJs}>
-          <Editor
-            options={options}
-            width="100%"
-            theme={"vs-dark"}
-            language={"javascript"}
-            defaultLanguage="javascript"
-            onChange={(value) => {
-              console.log(value);
-            }}
-          />
-        </SandBoxContainer>
+      <div>
+        <iframe
+          title="output"
+          className="output-window"
+          id="output-frame"
+        ></iframe>
       </div>
     </div>
   );
